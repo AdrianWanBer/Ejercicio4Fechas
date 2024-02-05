701b6e09889842b1b2fe3a66510c823e7c6c13a3
DESKTOP-3T85H9P\Adrian <csi23-awanuer@colegioaltair.net>
2024-02-05 12:34:01 +0100

    Agregar archivos de proyecto.

---
+++ Ejercicio4Fechas/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Ejercicio4Fechas
+{
+    class program
+    {
+        public static void Main(String[] args)
+        {
+            DateTime FechaActual = DateTime.Today;
+            Console.WriteLine("Cual es tu fecha de nacimiento (dd/mm/yyyy):");
+            string fechaNacimientoStr = Console.ReadLine();
+            DateTime fechaNacimiento = DateTime.Parse(fechaNacimientoStr);
+
+
+            int añosDiferencia = FechaActual.Year - fechaNacimiento.Year;
+            int mesesDiferencia = FechaActual.Month - fechaNacimiento.Month;
+            int diasDiferencia = FechaActual.Day - fechaNacimiento.Day;
+
+            if (mesesDiferencia < 0 || diasDiferencia < 0)
+            {
+                mesesDiferencia = mesesDiferencia + 12;
+                diasDiferencia = diasDiferencia + 31;
+                añosDiferencia = añosDiferencia - 1;
+            }
+
+            Console.WriteLine($"Tienes {añosDiferencia} años, {mesesDiferencia} meses y {diasDiferencia} dias.");
+
+        }
+    } 
+}
